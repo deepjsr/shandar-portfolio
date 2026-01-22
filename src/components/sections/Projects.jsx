6ecba9742578ffe11f6268819c53251c373424d3
deepjsr <sarma.deepjyoti481@gmail.com>
2026-01-22 20:34:09 +0530

    updated

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -13,6 +13,8 @@ function Projects() {
             Featured Projects
           </h2>
           <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+            {/* Project Card 1 */}
+
             <div className="p-6 rounded-xl border border-white/10% hover:translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition">
               <h3 className="text-xl font-bold mb-2">About React on Reddit</h3>
               <p className="text-gray-400 mb-4">
@@ -31,6 +33,36 @@ function Projects() {
               <div className="flex justify-between items-center">
                 <a
                   href="https://redit-react-posts.web.app/"
+                  target="_blank"
+                  className="text-blue-400 hover:text-blue-300 transition-colors my-4"
+                >
+                  View Project →
+                </a>
+              </div>
+            </div>
+            {/* Project Card 2 */}
+            <div className="p-6 rounded-xl border border-white/10% hover:translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition">
+              <h3 className="text-xl font-bold mb-2">
+                Crypto Trading Platfrom Prototype
+              </h3>
+              <p className="text-gray-400 mb-4">
+                plug and play crypto trading platfrom prototype
+              </p>
+              <div className="flex flex-wrap gap-2 mb-4">
+                {["Wordpress", "Elementor", "Royal Add Ons"].map(
+                  (tech, key) => (
+                    <span
+                      key={key}
+                      className="bg-blue-500/10 text-blue-500 py-1 px-3  rounded-full text-sm hover:bg-blue-500/20 transition-all  hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)]"
+                    >
+                      {tech}
+                    </span>
+                  ),
+                )}
+              </div>
+              <div className="flex justify-between items-center">
+                <a
+                  href="https://deeps.page.gd/?i=3"
                   target="_blank"
                   className="text-blue-400 hover:text-blue-300 transition-colors my-4"
                 >
