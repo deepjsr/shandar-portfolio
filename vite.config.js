d221d423183c3848e5300d2645506aa2b2d2aff3
deepjsr <sarma.deepjyoti481@gmail.com>
2026-02-05 11:06:14 +0530

    Fix : base path for custom domain

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,22 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-
 import { visualizer } from "rollup-plugin-visualizer";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    react(), 
+    react(),
     tailwindcss(),
     visualizer({
       open: true,
       gzipSize: true,
       brotliSize: true,
-    })
+    }),
   ],
-  base: "/shandar-portfolio",
+
+  //  FOR CUSTOM DOMAIN
+  base: "/",
+
   build: {
     rollupOptions: {
       output: {
